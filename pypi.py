3ad73444de6b70ff91a6d7db840488a1411b07ed
Sergey Alembekov <rt@aspirinka.net>
2011-11-05 15:44:51 +0300

    download and extract methods

--- pypi.py
+++ pypi.py
@@ -1,11 +1,13 @@
 import urllib2
 import simplejson
-
+import tarfile
+import sys
 
 class pypackage:
 	
-	def __init__(self,name):
+	def __init__(self,name,path="/tmp"):
 		self.name = str(name)
+		self.path = path
 		self.json_request = self.get_json_req()
 
 	def get_name(self):
@@ -35,7 +37,7 @@ class pypackage:
 			if meta['packagetype'] == 'sdist':
 				self.result.append(meta['filename'])
 		return self.result
-	
+
 	def get_version(self):
 		"""witch version of package in pypi we have"""
 		return simplejson.loads(self.json_request)["info"]['version']
@@ -50,3 +52,48 @@ class pypackage:
 				if meta['packagetype'] == 'sdist' and meta['filename'].find("tar") > -1:
 					self.result = meta['url']
 		return self.result
+
+	def get_most_freq_archive_filename(self):
+		self.result1 = []
+		for meta in simplejson.loads(self.json_request)["urls"]:
+			if len(meta) == 1 and meta['packagetype'] == 'sdist':
+				self.result1 = meta['filaname']
+			elif len(meta) > 1: 
+				if meta['packagetype'] == 'sdist' and meta['filename'].find("tar") > -1:
+					self.result1 = meta['filename']
+		return self.result1
+
+	def download(self):
+		"""get archive from pypi"""
+		self.filename = self.get_most_freq_archive_filename()
+		self.url = self.get_most_freq_archive_url()
+		self.filepath = self.path + "/" + self.filename
+		try:
+			self.f = urllib2.urlopen(self.url)
+			self.pypackage_file = open(self.filepath, "w")
+			self.pypackage_file.write(self.f.read())
+			self.pypackage_file.close()
+			return 1
+		except urllib2.HTTPError, e:
+			print "HTTP Error:",e.code , self.url
+		except urllib2.URLError, e:
+			print "URL Error:",e.reason , self.url
+        
+	def extract(self):
+		"""extract archive into path"""
+		try:
+			self.tar = tarfile.open(self.filepath)
+			#need to check if we can use tarfile module
+			tar.extractall(path=self.path)
+			tar.close()
+		except :
+			if self.download():
+				self.tar = tarfile.open(self.filepath)
+				tar.extractall(path=self.path)
+				tar.close()
+			else:
+				sys.exit(1)
+
+
+
+
